f00f0e5d2528d1647e46e4c7714675590df63cec
Bobiinette <nic1212c@gmail.com>
2017-05-17 15:52:29 +0200

    Bite2

--- app/controllers/annonce_controller.rb
+++ app/controllers/annonce_controller.rb
@@ -1,20 +1,23 @@
 class AnnonceController < ApplicationController
-  def create
-  	@annonces = Annonce.new annonce_params
-  	if @annonces.save
-  		flash[:alert] = "Votre annonce a bien été crée"
-  		redirect_to action:index
-  	else
-  		flash[:alert] = "Votre annonce n'a pas pu être crée"
-  		render "new"
-  	end
-  end
 
+  # GET /articles
+  # GET /articles.json
   def index
-  	@annonces = Annonce.paginate page: params[:page], per_page: 4
+  	@annonces = Annonce.all.paginate page: params[:page], per_page: 4
   end
 
   def show
+  end
+
+  def create
+    @annonces = Annonce.new(annonce_params)
+    if @annonces.save
+      flash[:alert] = "Votre annonce a bien été crée"
+      redirect_to action:index
+    else
+      flash[:alert] = "Votre annonce n'a pas pu être crée"
+      render "new"
+    end
   end
 
   private
